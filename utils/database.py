78df765b1904bc8c1f0fb1c81dcfbcd48d354a3a
ZaidDeAnda <zaidthejedi@gmail.com>
2022-09-25 15:31:23 -0500

    added functionality with streamlit secrets

--- utils/database.py
+++ utils/database.py
@@ -18,14 +18,10 @@ def get_mongo_client(config):
         client
             The mongo client connected to the database.
         """
-    db_mongo = config.get_config()['db_mongo']
-    user = db_mongo["user"]
-    password = urllib.parse.quote_plus(db_mongo["password"])
-    cluster = db_mongo["cluster"]
-    client = pymongo.MongoClient(
-        f"mongodb+srv://{user}:{password}@{cluster}/?retryWrites=true&w=majority"
-    )
-    return client
+    user = st.secrets["db_user"]
+    password = st.secrets["db_pwd"]
+    cluster = st.secrets["db_cluster"]
+    return pymongo.MongoClient(f"mongodb+srv://{user}:{password}@{cluster}/?retryWrites=true&w=majority")
 
 def create_dataframe_from_cursor(cursor):
     """Transform the cursor from mongo into a dataframe
